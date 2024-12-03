a8676c5f0b19d043a8c0f0cfb7f70b84a5aaab88
Jul1enF <j.furic@gmail.com>
2024-12-03 15:28:29 +0100

    Premier test dégradé vert pomme

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,45 @@
 import styles from '../styles/Home.module.css';
+import { useState, useEffect } from 'react'
+
+
+
 
 function Home() {
+
+
+  useEffect(() => {
+    setAnimationsBegin(true)
+    setTimeout(()=>setAnimations2Begin(true), 1600)
+  }, [])
+
+
+
+  const [animationsBegin, setAnimationsBegin] = useState(false)
+  const [animations2Begin, setAnimations2Begin] = useState(false)
+
+
+  const gradientBackgroundHeader = !animationsBegin ? styles.gradientBackgroundHeader1 : styles.gradientBackgroundHeader2
+
+
+  const headerGradientLine = !animationsBegin ? styles.headerGradientLine1 : styles.headerGradientLine2
+
+  const modal = !animations2Begin ? styles.modal1 : styles.modal2
+
+
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.body}>
+
+      <div className={styles.backgroundHeaderContainer}>
+        <div className={gradientBackgroundHeader}>
+          <h1 className={styles.title}>LOG ME UP</h1>
+          <h3 className={styles.name}>Julien Furic - Développeur d'applications web et mobile</h3>
+        </div>
+      </div>
+      <div className={headerGradientLine}></div>
+
+      <div className={modal}>
+
+      </div>
     </div>
   );
 }
