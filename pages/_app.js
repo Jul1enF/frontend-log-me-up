35d7df394233d387898b3712410eee23a93c02e6
Jul1enF <j.furic@gmail.com>
2025-09-01 13:20:51 +0200

    ajout de mots clefs SEO

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function App({ Component, pageProps }) {
         <meta name="description" content="Portfolio de Julien Furic, développeur web fullstack (Frontend + Backend)" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="Portfolio, Développeur, développeur web, Front-end, frontend, back-end, backend, fullstack, Julien, Furic" />
+        <meta name="keywords" content="Portfolio, Développeur, développeur web, Front-end, frontend, back-end, backend, fullstack, Julien, Furic, Next, Next.js, Javascript, TypeScript, CSS, Expo, expo, react-native, redux, express, Express, MongoDB, node, vercel, Vercel, react, React" />
         <link rel="icon" href="/logo-9-portfolio.png" />
         {/* Fond status bar pour téléphones : */}
         <meta name="theme-color" content="070508"></meta>
